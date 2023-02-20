b87005e7338d95eb60c1515f792855eb0b072577
jjjmc <jmc879855740@qq.com>
2023-02-20 23:29:11 +0800

    [feat]change appkey

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const LIMIT_AI_IMAGE_COUNT = 5;
 const LIMIT_COUNT_RESPONSE = '对不起，因为ChatGPT调用收费，您的免费使用额度已用完~'
 
 const configuration = new Configuration({
-  apiKey: '##your api key##',
+  apiKey: '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',
 });
 
 const openai = new OpenAIApi(configuration);
